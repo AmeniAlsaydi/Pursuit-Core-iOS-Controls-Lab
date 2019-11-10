4698cb385cabb3e1e3c2ad764e372527ab1fce80
Ameni Alsaydi <amenialsaydi@pursuit.org>
2019-11-10 15:39:26 -0500

    card generator fully functional

--- ControlsLab/ControlsLab/ViewController.swift
+++ ControlsLab/ControlsLab/ViewController.swift
@@ -13,13 +13,36 @@ class ViewController: UIViewController {
     @IBOutlet weak var middleLabel: UILabel!
     @IBOutlet weak var rightLabel: UILabel!
     
-    
     var suit: Suit = .heart
+    var card: String = "A"
+
+
+    var currentSegmentIndex: Int = 0 {
+        didSet {
+            switch suitChanger.selectedSegmentIndex {
+            case 0:
+                suit = .diamond
+            case 1:
+                suit = .heart
+            case 2:
+                suit = .club
+            case 3:
+                suit = .spade
+            default:
+               print("")
+            }
+
+        }
+    }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        leftLabel.text = "♦️"
+        configuresStepper()
+        leftLabel.text = "\(card)\(suit.rawValue)"
+        middleLabel.text = "\(card)\(suit.rawValue)"
+        rightLabel.text = "\(card)\(suit.rawValue)"
+        
     }
     
     func configuresStepper() {
@@ -31,38 +54,45 @@ class ViewController: UIViewController {
         valueChanger.value = 1
     }
     
-    
+  // ✅ WORKS AS DESIRED
     @IBAction func segmentedControlChange(_ sender: UISegmentedControl) {
         
-        // currentSegmentIndex = sender.selectedSegmentIndex // valid values 0, 1, 2, 3 -> just gets the segment value
+       currentSegmentIndex = sender.selectedSegmentIndex // valid values 0, 1, 2, 3 -> just gets the segment value
         
-        switch sender.selectedSegmentIndex {
-        case 0:
-            suit = .diamond
-            //leftLabel.text = "♦️"
-        case 1:
-            suit = .heart
-           //leftLabel.text = "❤️"
-        case 2:
-            suit = .club
-            //leftLabel.text = "♣️"
-        case 3:
-            suit = .spade
-            //leftLabel.text = "♠️"
-        default:
-           print("")
-        }
-        
-        leftLabel.text = "1 \(suit.rawValue)"
+        leftLabel.text = "\(card)\(suit.rawValue)"
+        middleLabel.text = "\(card)\(suit.rawValue)"
+        rightLabel.text = "\(card)\(suit.rawValue)" 
     }
     
     
     @IBAction func valueChanged(_ sender: UIStepper) {
         
+        let cardNum = sender.value
+        let cardNumAsInt = Int(cardNum)
         
+        switch cardNumAsInt {
+        case 1:
+            card = "A"
+        case 2...10:
+            card = cardNumAsInt.description
+        case 11:
+            card = "J"
+        case 12:
+            card = "Q"
+        case 13:
+            card = "K"
+        default:
+            print("")
+        }
+        
+        
+        leftLabel.text = "\(card)\(suit.rawValue)"
+        middleLabel.text = "\(card)\(suit.rawValue)"
+        rightLabel.text = "\(card)\(suit.rawValue)"
     }
     
     
     
 }
+
 
